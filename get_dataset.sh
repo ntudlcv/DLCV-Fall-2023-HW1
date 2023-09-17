89f7ccb1a6f51c48ffabfa67e611ed2528b66c21
yanghan9014 <yanghan9014@gmail.com>
2023-09-17 06:52:58 +0000

    update link

--- get_dataset.sh
+++ get_dataset.sh
@@ -1,6 +1,5 @@
 # Download dataset
-gdown 1tlqGACUfVuG2h-vamSidy6sALjvgsN42 -O hw1_data.zip
+gdown 1YLKmqlNPHNCOm-qaKq6XD_GWgOeOloOT -O hw1_data.zip
 
 # Unzip the downloaded zip file
-mkdir hw1_data
 unzip ./hw1_data.zip
